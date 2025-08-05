4ba606ebb63f02680a257123056703de042971a6
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-06 01:01:16 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import Task from "./js/core/taskClass";
+import setupScrollbars from "./js/ui/scrollbar";
+import initialiseSidebar from "./js/ui/sidebar";
 import "./styles.css";
-import displayJSON from "./js/core/displayJSON";
-import TaskManager from "./js/core/taskManager";
 
 console.log("Application initialised!");
 
-const taskManager = new TaskManager();
+setupScrollbars();
+initialiseSidebar();
 
-const task = new Task("Model Training", "Learn PyTorch", "10:30AM - 11/2/2025", "High", "Its really complex, must give a lot of time.");
-taskManager.removeTask("4b0f224a-040c-404d-904e-88430f1badef", "4234e254-23cd-4ca8-92ba-5b58b9fb03f6", task);
-taskManager.renameProject("4b0f224a-040c-404d-904e-88430f1badef", "Artificial Intelligence");
-
-displayJSON(taskManager.getAllData());
---
+++ src/js/ui/sidebar.js
@@ -0,0 +1,66 @@
+import sidebarIcon from "../../icons/sidebar_icon.svg";
+import addIcon from "../../icons/add_icon.svg";
+import todayIcon from "../../icons/today_icon.svg";
+import upcomingIcon from "../../icons/upcoming_icon.svg";
+import completedIcon from "../../icons/completed_icon.svg";
+import redHashtagIcon from "../../icons/red_hashtag_icon.svg";
+import blueHashtagIcon from "../../icons/blue_hashtag_icon.svg";
+import greenHashtagIcon from "../../icons/green_hashtag_icon.svg";
+import projectAddButton from "../../icons/project_add_button.svg";
+import blackHashtagIcon from "../../icons/black_hashtag_icon.svg";
+
+
+export default function initialiseSidebar() {
+    const sidebarElement = document.querySelector(".sidebar");
+    sidebarElement.innerHTML = /* html */ `
+        <div class="sidebar-heading">
+            <div class="brand-name">Polymath</div>
+            <img class="sidebar-icon" src="${sidebarIcon}" alt="Toggle Sidebar">
+        </div>
+        <div class="add-task-button">
+            <img class="add-icon" src="${addIcon}">
+            Add Task
+        </div>
+        <div class="sidebar-item">
+            <img class="sidebar-item-icon" src="${todayIcon}">
+            Today
+        </div>
+        <div class="sidebar-item">
+            <img class="sidebar-item-icon" src="${upcomingIcon}">
+            Upcoming
+        </div>
+        <div class="sidebar-item">
+            <img class="sidebar-item-icon" src="${completedIcon}">
+            Completed
+        </div>
+        <div class="sidebar-section-name">Priority</div>
+        <div class="sidebar-item">
+            <img class="sidebar-item-icon" src="${redHashtagIcon}">
+            High
+        </div>
+        <div class="sidebar-item">
+            <img class="sidebar-item-icon" src="${blueHashtagIcon}">
+            Medium
+        </div>
+        <div class="sidebar-item">
+            <img class="sidebar-item-icon" src="${greenHashtagIcon}">
+            Low
+        </div>
+        <div class="sidebar-section-name project-section">
+            Projects
+            <img class="project-section-add-button" src="${projectAddButton}" alt="Add Projects">
+        </div>
+        <div class="sidebar-item">
+            <img class="sidebar-item-icon" src="${blackHashtagIcon}">
+            Weather App
+        </div>
+        <div class="sidebar-item">
+            <img class="sidebar-item-icon" src="${blackHashtagIcon}">
+            Backend
+        </div>
+        <div class="sidebar-item">
+            <img class="sidebar-item-icon" src="${blackHashtagIcon}">
+            DSA
+        </div>
+    `;
+}
