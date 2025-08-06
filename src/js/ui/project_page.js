0c870294e2532140fdd4ccd9fb511df9499447c1
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-06 14:33:51 +0530

    project page starting

--- src/js/ui/project_page.js
+++ src/js/ui/project_page.js
@@ -1,3 +1,25 @@
+import TaskManager from "../core/taskManager";
+import menuIcon from "../../icons/menu_icon.svg";
+
 export default function createProjectPage(projectID) {
+    const taskManager = new TaskManager();
+    const projects = taskManager.getAllProjects();
+    const project = projects.find((project) => project.projectID == projectID);
+
+    const contentElement = document.querySelector(".content");
+    contentElement.classList.add("project-page");
+    contentElement.innerHTML = /* html */ `
+        <div class="project-page-header">
+            <div class="project-name">${project.projectName}</div>
+            <img class="project-menu" src="${menuIcon}">
+        </div>
+        <div class="tasks">
+
+        </div>
+        <div class="add-task-item">
+        Add Task
+        </div>
+    `;
+
     console.log("Project Page created for ID : ", projectID);
 }
