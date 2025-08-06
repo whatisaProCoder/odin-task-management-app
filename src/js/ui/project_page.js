5e61d737f18efb3b8c96057a2aea76308713e492
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-06 19:31:09 +0530

    task item design done

--- src/js/ui/project_page.js
+++ src/js/ui/project_page.js
@@ -1,5 +1,16 @@
 import TaskManager from "../core/taskManager";
 import menuIcon from "../../icons/menu_icon.svg";
+import blueAddIcon from "../../icons/blue_add_icon.svg";
+import redHashtagIcon from "../../icons/red_hashtag_icon.svg";
+import blueHashtagIcon from "../../icons/blue_hashtag_icon.svg";
+import greenHashtagIcon from "../../icons/green_hashtag_icon.svg";
+import redAlarmIcon from "../../icons/red_alarm_icon.svg";
+import blueAlarmIcon from "../../icons/blue_alarm_icon.svg";
+import greenAlarmIcon from "../../icons/green_alarm_icon.svg";
+
+const highPriorityColor = "#FF5353";
+const mediumPriorityColor = "#5C53FF";
+const lowPriorityColor = "#08C81F";
 
 export default function createProjectPage(projectID) {
     const taskManager = new TaskManager();
@@ -14,10 +25,23 @@ export default function createProjectPage(projectID) {
             <img class="project-menu" src="${menuIcon}">
         </div>
         <div class="tasks">
-
+            <div class="task-card">
+                <div class="upper-row">
+                    <input type="checkbox" id="task-checkbox" class="task-card-checkbox" style="accent-color: ${highPriorityColor}">
+                    <img class="task-hashtag-icon" src="${redHashtagIcon}">
+                    <div class="task-title">
+                        Brainstorm the features the app will have
+                    </div>
+                </div>
+                <div class="lower-row">
+                    <img class="task-alarm-icon" src="${redAlarmIcon}">
+                    <div class="task-due-date">07:30 PM - 04/08/2025</div>
+                </div>
+            </div>
         </div>
         <div class="add-task-item">
-        Add Task
+            <img class="blue-add-icon" src="${blueAddIcon}">
+            Add Task
         </div>
     `;
 
