ea4c4d7c343e0538db5802c65a9ba345341a2144
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-08 17:06:40 +0530

    welcome page modified

--- src/js/ui/welcome_page.js
+++ src/js/ui/welcome_page.js
@@ -3,7 +3,8 @@ export default function createWelcomePage() {
     content.innerHTML = /* html */ `
         <div class="welcome-page">
             <div class="text">Welcome to <span>Polymath</span></div> 
-            <div class="subtext">An intuitive task management application</div>
+            <div class="subtext">An intuitive task management application.</div>
+            <div class="user-prompt">Start by adding a project and task!</span>
         </div>
     `;
 }
