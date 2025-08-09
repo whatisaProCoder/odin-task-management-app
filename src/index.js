b3b8e5eb5fb3ca91adf9f92040e34ea56840fdbc
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-09 13:16:13 +0530

    finish

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import initialiseSidebar from "./js/ui/sidebar";
 import createWelcomePage from "./js/ui/welcome_page";
 import initialiseAddTaskDialogBox from "./js/ui/add_task_dialog";
 import initialiseEditTaskDialogBox from "./js/ui/edit_task_dialog";
+import createTitleBar from "./js/ui/electron_custom_title_bar";
+import initialisePreviewTaskDialogBox from "./js/ui/preview_task_dialog";
 
 initialiseAddTaskDialogBox();
 initialiseEditTaskDialogBox();
@@ -18,11 +20,12 @@ setupScrollbars();
 console.log("Application successfully initialised!");
 
 // if building electron app
-import createTitleBar from "./js/ui/electron_custom_title_bar";
-import initialisePreviewTaskDialogBox from "./js/ui/preview_task_dialog";
-document.body.style.paddingTop = "40px";
-document.body.style.borderRadius = "0.5rem";
-createTitleBar();
+const buildingElectronApp = true;
+if (buildingElectronApp) {
+    document.body.style.paddingTop = "40px";
+    document.body.style.borderRadius = "0.5rem";
+    createTitleBar();
+}
 
 
 
