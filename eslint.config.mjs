92263435b03ceca03d7a83f1dc746e72acd0ff7f
whatisaProCoder <debnathpritam2005@gmail.com>
2025-08-11 11:18:23 +0530

    Add Husky pre-commit hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ["js/recommended", "prettier"],
     languageOptions: { globals: globals.browser },
   },
 ]);
